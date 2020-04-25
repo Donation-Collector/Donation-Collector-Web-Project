5d20d2b8a82ac5e10512c477912a350b54ea492c
Gilbertie <wangqihao264@gmail.com>
2020-04-24 22:13:45 -0400

    modified:   src/main/java/com/example/dc/model/NGO.java

--- src/main/java/com/example/dc/model/NGO.java
+++ src/main/java/com/example/dc/model/NGO.java
@@ -16,7 +16,7 @@ public class NGO {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String name;
 
     @Column(nullable = false)
