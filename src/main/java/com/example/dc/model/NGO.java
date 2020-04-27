1f9d38acc96e237ee57ff75172d98943b7bcf562
yyfMichaelYan <yyfmichaelyan@gmail.com>
2020-04-27 00:15:44 -0500

    update NGO

--- src/main/java/com/example/dc/model/NGO.java
+++ src/main/java/com/example/dc/model/NGO.java
@@ -35,4 +35,77 @@ public class NGO {
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "address_id", nullable = false)
     private Address address;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getCoverArea() {
+        return coverArea;
+    }
+
+    public void setCoverArea(String coverArea) {
+        this.coverArea = coverArea;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
 }
--- src/main/java/donation/ngo/NgoRepository.java
+++
@@ -1,7 +0,0 @@
-package donation.ngo;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface NgoRepository extends CrudRepository<Ngo, String>{
-
-}
