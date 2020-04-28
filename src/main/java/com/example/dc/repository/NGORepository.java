e0a651f943f0287e560a7fb326453ebaf99f1cfc
cymprain <62127786+cymprain@users.noreply.github.com>
2020-04-27 22:18:30 -0700

    Delete NGORepository created by Ying

--- src/main/java/com/example/dc/repository/NGORepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.dc.repository;
-
-import com.example.dc.model.NGO;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NGORepository extends CrudRepository<NGO, Long> {
-}
